f37bf6f58995ec8996edc39924360ba0eabd8d64
David Robert <contact@david-robert.fr>
2024-11-12 10:15:50 +0100

    router

---
+++ controller/IndexController.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('../model/Order.php');
+
+
+class IndexController {
+
+
+    public function index() {
+        $message = null;
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            if (key_exists('customerName', $_POST)) {
+
+                try {
+                    $order = new Order($_POST['customerName']);
+                    $message = 'Commande créée';
+                } catch (Exception $exception) {
+                    $message = $exception->getMessage();
+                }
+
+            }
+        }
+
+
+        require_once('../view/index-view.php');
+    }
+
+}
+
---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('../controller/IndexController.php');
+
+// récupère l'url actuelle
+$requestUri = $_SERVER['REQUEST_URI'];
+
+// découpe l'url actuelle pour ne récupérer que la fin
+// si l'url demandée est "http://localhost:8888/piscine-ecommerce-app/public/test"
+// $enduri contient "test"
+$uri = parse_url($requestUri, PHP_URL_PATH);
+$endUri = str_replace('/piscine-ecommerce-app/public/', '', $uri);
+$endUri = trim($endUri, '/');
+
+
+// en fonction de la valeur de $endUri on charge le bon contrôleur
+if ($endUri === "") {
+    $indexController = new IndexController();
+    $indexController->index();
+}
